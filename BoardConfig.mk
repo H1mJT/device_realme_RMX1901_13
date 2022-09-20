15d06cfd860ea2fbf54464dbb32e782a50b87359
H1mJT <himjyoti22@gmail.com>
2022-09-20 11:00:30 -0400

    Required because LLVM binutils is used by default now https://github.com/DerpFest-AOSP/vendor_derp/commit/76f6834d33c1ee360d4bce46b7133563f80d9138

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -46,6 +46,7 @@ TARGET_KERNEL_SOURCE := kernel/realme/RMX1901
 TARGET_KERNEL_CONFIG := RMX1901_defconfig
 TARGET_KERNEL_CLANG_VERSION := proton
 TARGET_KERNEL_CLANG_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-proton
+TARGET_KERNEL_LLVM_BINUTILS := false
 TARGET_KERNEL_ADDITIONAL_FLAGS += \
     HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"
 
