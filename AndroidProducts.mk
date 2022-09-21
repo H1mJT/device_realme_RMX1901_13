e13e4a0a9df80ca0f1deab5db3a03b3e6f47c7be
H1mJT <himjyoti22@gmail.com>
2022-09-21 10:08:37 -0400

    Ricedroid bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/derp_RMX1901.mk
+    $(LOCAL_DIR)/lineage_RMX1901.mk
 
 COMMON_LUNCH_CHOICES := \
-    derp_RMX1901-userdebug \
-    derp_RMX1901-eng
+    lineage_RMX1901-userdebug \
+    lineage_RMX1901-eng
 
--- lineage_RMX1901.mk
+++ lineage_RMX1901.mk
@@ -10,14 +10,35 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 
 # Inherit some common derp stuff.
-$(call inherit-product, vendor/derp/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# maintainer flag (one word only/no spacing)
+RICE_MAINTAINER := HimJT
+
+# chipset flag (one word only/no spacing)
+RICE_CHIPSET := sdm710
+
+# gapps build flag, if not defined build type is vanilla
+WITH_GMS := true
+
+# Sushi Bootanimation (only 720/1080p/1440 supported. if not defined, bootanimation is google bootanimation)
+SUSHI_BOOTANIMATION := 1080
+
+# disable/enable blur support, default is false
+TARGET_ENABLE_BLUR := true
+
+# UDFPS ICONS/ANIMATIONS
+TARGET_HAS_UDFPS := true
 
 
-# Inherit some common DerpFest stuff
-TARGET_BOOT_ANIMATION_RES := 1080
-USE_LEGACY_BOOTANIMATION := true
-EXTRA_UDFPS_ANIMATIONS := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+TARGET_SUPPORTS_QUICK_TAP := true
+WITH_FOD_ANIMATIONS := true
 TARGET_USES_BLUR := true
+TARGET_WANTS_FOD_ANIMATIONS := true
+EXTRA_UDFPS_ANIMATIONS := true
+
 
 
 # Inherit from RMX1901 device
@@ -26,7 +47,7 @@ $(call inherit-product, $(LOCAL_PATH)/device.mk)
 PRODUCT_BRAND := Realme
 PRODUCT_DEVICE := RMX1901
 PRODUCT_MANUFACTURER := Realme
-PRODUCT_NAME := derp_RMX1901
+PRODUCT_NAME := lineage_RMX1901
 PRODUCT_MODEL := Realme X
 
 PRODUCT_GMS_CLIENTID_BASE := android-oppo
