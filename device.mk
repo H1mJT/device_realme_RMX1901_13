f8bf32391ece001dbdc8a8a7173912f5aab67bad
Mathieu Chartier <>
2022-11-20 13:19:25 +0530

    sdm710-common: Speed profile services and wifi-service to reduce RAM and storage
    Change-Id: I1d907b24cb2760ddd9cc65ac4335730c8e61a0a3
    Signed-off-by: clarencelol <clarencekuiek@icloud.com>
    Signed-off-by: kssrao13882 <kssrao13882@gmail.com>

--- device.mk
+++ device.mk
@@ -435,6 +435,9 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-service.realme_sdm710 \
     libsensorndkbridge
 
+# Speed profile services and wifi-service to reduce RAM and storage
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
