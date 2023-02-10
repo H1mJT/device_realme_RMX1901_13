dc0d0a98a8f3b4a18d1ed972954621ad3f90e1e3
H1mJT <himjyoti22@gmail.com>
2023-02-10 14:36:47 +0530

    Revert "Revert "RMX1901: [TEMP] Nuked Realme Dirac""
    This reverts commit 206341e9457caa5ebeaf6863a2ed9f07b0b11446.

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,23 +1,19 @@
 # Vendor Tree
-echo 'Cloning Vendor tree [1/5]'
-git clone git@github.com:H1mJT/vendor_realme_RMX1901.git vendor/realme/RMX1901
+echo 'Cloning Vendor tree [1/4]'
+git clone git@github.com:H1mJT/vendor_realme_RMX1901_13.git vendor/realme/RMX1901
 
 
 #Kernel tree
-echo 'Cloning Kernel tree [2/5]'
+echo 'Cloning Kernel tree [2/4]'
 git clone git@github.com:H1mJT/kernel_realme_RMX1901.git -b upstream kernel/realme/RMX1901
 
 
 # Use Proton Clang For Compilation
-echo ' Using Proton Clang For Compilation [3/5]'
+echo ' Using Proton Clang For Compilation [3/4]'
 git clone --depth=1 https://github.com/kdrag0n/proton-clang.git prebuilts/clang/host/linux-x86/clang-proton
 
 
-# Realme Dirac
-echo 'Adding Realme Dirac Support [4/5]'
-git clone git@github.com:H1mJT/realme_dirac packages/apps/RealmeDirac
-
-
-echo 'Patching Realme In-Screen Proximity [5/5]'
+echo 'Patching Realme In-Screen Proximity [4/4]'
 # Realme In-Screen Proximity Patch
 cd frameworks/base && git fetch git@github.com:H1mJT/proximity_patch.git && git cherry-pick de630e735cdbb68a606d3bbbf8b1674be31bab2d && git cherry-pick 6c0e9b899bef7b4681b5f0dc7110ca76b35b9a74 && git cherry-pick 48541697c7d13d3bb75633a88382e31cf0bdefb9 && cd ../..
+
