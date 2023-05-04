aa56fc2f2cac8a96168bf2fa5f604d75b5315d25
jro1979oliver <jroliveira.oliveira301@gmail.com>
2023-05-04 19:35:23 +0200

    RMX1901: Cleanup kernel flags

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -48,8 +48,6 @@ TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_CLANG_VERSION := proton
 TARGET_KERNEL_CLANG_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-proton
 TARGET_KERNEL_LLVM_BINUTILS := false
-TARGET_KERNEL_ADDITIONAL_FLAGS += \
-    HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"
 
 
 
