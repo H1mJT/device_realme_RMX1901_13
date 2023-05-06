ca3854b899df6fe301a55e003c365618df211941
AmeChanRain <downloadbot007@gmail.com>
2023-05-06 23:21:58 +0530

    Add empty Android.mk file to avoid conflict
    * build/make/core/base_rules.mk:338: error: kernel/KernelSU/userspace/su/jni: MODULE.TARGET.EXECUTABLES.su already defined by system/extras/su.

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,11 +5,11 @@ git clone git@github.com:H1mJT/vendor_realme_RMX1901_13.git vendor/realme/RMX190
 
 #Kernel tree
 echo 'Cloning Kernel tree [2/4]'
-git clone --recurse-submodules git@github.com:H1mJT/kernel_realme_RMX1901.git -b upstream_ksu kernel/realme/RMX1901
+git clone --recurse-submodules git@github.com:H1mJT/kernel_realme_RMX1901.git -b x.337_ksu kernel/realme/RMX1901
 
-#Syncing KernelSU to latest source
+#Syncing KernelSU to latest source and avoid a conflict
 echo "Syncing KernelSU to latest source [3/4]"
-cd kernel/realme/RMX1901 && git submodule sync && git submodule update --init --recursive && cd KernelSU && git checkout main && git pull && cd ../../../..
+cd kernel/realme/RMX1901 && git submodule sync && git submodule update --init --recursive && cd KernelSU && git checkout main && git pull && touch Android.mk && cd ../../../..
 
 echo 'Patching Realme In-Screen Proximity [4/4]'
 # Realme In-Screen Proximity Patch
